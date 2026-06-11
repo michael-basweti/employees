f497e25a955f25824139bdc63c0415cb617b4081
michael-basweti <baswetima@gmail.com>
2026-06-11 20:39:07 +0300

    update employee

--- src/main/java/com/mbasweti/springboot/employees/controller/EmployeeRestController.java
+++ src/main/java/com/mbasweti/springboot/employees/controller/EmployeeRestController.java
@@ -2,11 +2,11 @@ package com.mbasweti.springboot.employees.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -56,6 +56,15 @@ public class EmployeeRestController {
     public Employee addEmployee(@Valid @RequestBody EmployeeRequest theEmployee) {
         Employee dbEmployee = employeeService.save(theEmployee);
         return dbEmployee;
+    }
+
+
+    @Operation(summary = "Update employee", description = "Updates an existing employee")
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{employeeId}")
+    public Employee updateEmployee(@PathVariable @Min(value = 1, message = "Employee ID must be a positive integer") long employeeId,
+            @Valid @RequestBody EmployeeRequest theEmployee) {
+        return employeeService.update(employeeId, theEmployee);
     }
 
 }
