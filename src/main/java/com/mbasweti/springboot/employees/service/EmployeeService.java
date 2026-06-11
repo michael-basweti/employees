ba5c31b6152483bfbc2da263976c50a0a7de554f
michael-basweti <baswetima@gmail.com>
2026-06-11 16:12:16 +0300

    Implement employee service CRUD

--- src/main/java/com/mbasweti/springboot/employees/service/EmployeeService.java
+++ src/main/java/com/mbasweti/springboot/employees/service/EmployeeService.java
@@ -3,9 +3,21 @@ package com.mbasweti.springboot.employees.service;
 import java.util.List;
 
 import com.mbasweti.springboot.employees.entity.Employee;
+import com.mbasweti.springboot.employees.request.EmployeeRequest;
+
 
 public interface EmployeeService {
 
     List<Employee> findAll();
-    
+
+    Employee findById(int theId);
+
+    Employee save(EmployeeRequest theEmployeeRequest);
+
+    Employee update(long id, EmployeeRequest theEmployeeRequest);
+
+    Employee convertToEmployee(long id, EmployeeRequest theEmployeeRequest);
+
+    void deleteById(int theId);
+
 }
--- src/main/java/com/mbasweti/springboot/employees/service/EmployeeServiceImplementation.java
+++ src/main/java/com/mbasweti/springboot/employees/service/EmployeeServiceImplementation.java
@@ -3,9 +3,11 @@ package com.mbasweti.springboot.employees.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mbasweti.springboot.employees.dao.EmployeeDAO;
 import com.mbasweti.springboot.employees.entity.Employee;
+import com.mbasweti.springboot.employees.request.EmployeeRequest;
 
 @Service
 public class EmployeeServiceImplementation implements EmployeeService {
@@ -21,5 +23,44 @@ public class EmployeeServiceImplementation implements EmployeeService {
         return employeeDAO.findAll();
     }
 
+    @Override
+    public Employee findById(int theId) {
+        Employee theEmployee = employeeDAO.findById(theId);
+        
+        return theEmployee;
+    }
+
+    @Transactional
+    @Override
+    public Employee save(EmployeeRequest theEmployeeRequest) {
+        Employee theEmployee = convertToEmployee(0, theEmployeeRequest);
+        Employee dbEmployee = employeeDAO.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @Transactional
+    @Override
+    public Employee update(long id, EmployeeRequest theEmployeeRequest) {
+        Employee theEmployee = convertToEmployee(id, theEmployeeRequest);
+        Employee dbEmployee = employeeDAO.save(theEmployee);
+        return dbEmployee;
+    }
+
+    @Override
+    public Employee convertToEmployee(long id, EmployeeRequest theEmployeeRequest) {
+        Employee theEmployee = new Employee();
+        theEmployee.setId(id);
+        theEmployee.setFirstName(theEmployeeRequest.getFirstName());
+        theEmployee.setLastName(theEmployeeRequest.getLastName());
+        theEmployee.setEmail(theEmployeeRequest.getEmail());
+        return theEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
+    }
+
+    
+
 }
-   
