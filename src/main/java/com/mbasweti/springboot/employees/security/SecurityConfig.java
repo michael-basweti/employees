4c9a691da8495bc0fedb32fbafaac38c9374508a
michael-basweti <baswetima@gmail.com>
2026-06-18 12:25:31 +0300

    spring security request mappers

--- src/main/java/com/mbasweti/springboot/employees/security/SecurityConfig.java
+++ src/main/java/com/mbasweti/springboot/employees/security/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.mbasweti.springboot.employees.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -31,6 +35,29 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers("/docs/**","/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+                );
+        
+        
+        // Use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // Disable CSRF protection since we are not using cookies for session tracking
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+       
     }
 
 }
